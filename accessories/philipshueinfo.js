824fe335f9cf1ef0b93fc6eb995e7662bf2d0a2f
brianantonelli <brian.antonelli@gmail.com>
2015-01-14 03:48:43 +0000

    Adding my accessories;

---
+++ accessories/philipshueinfo.js
@@ -0,0 +1,7 @@
+var exports = module.exports = {};
+
+exports.philipsHueIP = "10.0.1.2";
+exports.philipsHueUsername = "newdeveloper";
+	exports.philipsHueManufacturer = "Philips"; //The manufacturer for the lights, not very important
+exports.philipsHueDevicePin = "031-45-154"; //The pin you have to enter when connecting the homekit light
+
